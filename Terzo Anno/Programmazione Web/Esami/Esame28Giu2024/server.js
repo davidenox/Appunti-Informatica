4cf99c9d4df6245106991cc42f357e5cdbae5afb
Davide <nocedav31@gmail.com>
2024-09-16 15:29:12 +0200

    esame28Giu2024

---
+++ Terzo Anno/Programmazione Web/Esami/Esame28Giu2024/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+const dataPath = './persone.json';
+
+app.get('/persone', (req, res) => {
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading file');
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.get('/persone/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      if (err) {
+        res.status(500).send('Error reading file');
+        return;
+      }
+      const persone = JSON.parse(data);
+      const persona = persone.find(p => p.id == id);
+      if (persona) {
+        res.json(persona);
+      } else {
+        res.status(404).send('Person not found');
+      }
+    });
+  });
+
+app.listen(port, () =>{
+    console.log(`Server running on http://localhost:${port}`);
+})
