ad1fe42c3e628d274d33b0dcd3e6a2f55ff894e7
Davide <nocedav31@gmail.com>
2024-09-20 11:38:57 +0200

    vault backup: 2024-09-20 11:38:57

--- Terzo Anno/Programmazione Web/Esami/Esame20Sett2024/script.js
+++ Terzo Anno/Programmazione Web/Esami/Esame20Sett2024/script.js
@@ -1,15 +1,28 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    fetch('http://localhost:3000/data')
+
+    // Prima richiesta
+    fetch('http://localhost:3000/task')
     .then(response => response.json())
     .then(data => {
-        const container = document.getElementById('container');
+        const main = document.getElementById('main');
         data.forEach(item => {
+            const btn = document.createElement('button');
             const card = document.createElement('div');
             card.classList.add('card');
             card.innerHTML = `
-            <h2>${item.name}</h2>
+            <h2>${item.text}</h2>
+            <button> Completa </button>
             `;
-            container.appendChild(card);
+            if(item.completed == true){
+                //btn.classList.add('btnCompleted')
+                card.innerHTML = `
+                <h2>${item.text}</h2>
+                <button style="backgound-color:grey"> Completa </button>
+                `;
+            }
+
+            //main.appendChild(btn)
+            main.appendChild(card);
             });
         })
 });
--- Terzo Anno/Programmazione Web/Esami/Esame20Sett2024/server.js
+++ Terzo Anno/Programmazione Web/Esami/Esame20Sett2024/server.js
@@ -6,19 +6,26 @@ const app = express();
 const port = 3000;
 app.use(cors());
 
-app.get('/data', (req,res)=>{
-    const data = fs.readFileSync('data.json', 'utf8');
-    res.json(JSON.parse(data));
+app.get('/task', (req,res)=>{
+    const task = fs.readFileSync('task.json', 'utf8');
+    res.json(JSON.parse(task));
     
 });
 
-app.get('/data/:id', (req,res)=>{
+app.get('/task/complete/:id', (req,res)=>{
     const id = req.params.id;
-    const data = fs.readFileSync('data.json', 'utf8');
-    const jsonData = JSON.parse(data);
+    const task = fs.readFileSync('task.json', 'utf8');
+    const jsonData = JSON.parse(task);
     const item = jsonData.find(item => item.id === parseInt(id));
     if(item){
-        res.json(item);
+        // jsonData.forEach(item =>{
+            // const itemCompleted = jsonData.find(item => item.completed == true);
+            // if(itemCompleted){
+            //     res.json(itemCompleted);
+            // }
+            
+        // })
+        res.json(item)
     }else{
         res.status(404).json({message: 'Item not found'});
     }
